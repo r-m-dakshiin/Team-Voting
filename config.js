339a5eab76de33efb29604d6ad267c127f911674
r-m-dakshiin <79435405+r-m-dakshiin@users.noreply.github.com>
2021-09-08 19:30:23 +0530

    config.js

--- config.js
+++ config.js
@@ -1,9 +1,17 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
-
-};
+const firebaseConfig = {
+    apiKey: "AIzaSyC3TL6bwzigD9RK9LWYra1w7JocBTVqrZc",
+    authDomain: "team-voting-2ff0a.firebaseapp.com",
+    databaseURL: "https://team-voting-2ff0a-default-rtdb.firebaseio.com",
+    projectId: "team-voting-2ff0a",
+    storageBucket: "team-voting-2ff0a.appspot.com",
+    messagingSenderId: "662650884685",
+    appId: "1:662650884685:web:f2717fd24b6521a4278204",
+    measurementId: "G-E6MDTNNSH6"
+  };
+  
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
